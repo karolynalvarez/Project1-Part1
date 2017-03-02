c688b5441f8ffa07fe0c41e7234dd9a83a4e7089
Karolyn Alvarez <karolynalvarez@karolyns-mbp.attlocal.net>
2017-03-01 22:53:54 -0800

    xcode project start

---
+++ library/library/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  library
+//
+//  Created by Karolyn Alvarez on 3/1/17.
+//  Copyright © 2017 Karolyn Alvarez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
