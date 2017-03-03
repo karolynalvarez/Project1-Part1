6d45524c2ddb0f991a6a2ffb6e363c013a16703f
Karolyn Alvarez <karolynalvarez@karolyns-mbp.attlocal.net>
2017-03-02 22:47:04 -0800

    project nearly completed

--- library/library/main.cpp
+++ library/library/main.cpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -45,24 +46,186 @@ public:
     
 };
 
-int main() {
-    Book book1;
-    string newtitle;
-    string newAuthor;
-    string newDescription;
+class VHS {
+private:
+    string title;
+    string description;
+public:
+    void setTitle (string _title ) {
+        title = _title;
+    }
+    string getTitle(){
+        return title;
+    }
     
-    cout << "Please enter the title for the book: ";
-    cin >> newtitle;
-    book1.setTitle(newtitle);
+    void setDescription (string _description ) {
+        description = _description;
+    }
+    string getDescription(){
+        return description;
+    }
+    void displayVHS(){
+        cout << "Title: " << title << endl;
+        cout << "Description: " << description << endl;
+    };
     
-    cout << "Please enter the author for the book: ";
-    cin >> newAuthor;
-    book1.setAuthor(newAuthor);
+};
+
+class Magazine {
+private:
+    string title;
+    string publisher;
+    string yearIssued;
     
-    cout << "Please enter the description for the book: ";
-    cin >> newDescription;
-    book1.setDescription(newDescription);
+public:
+    void setTitle (string _title ) {
+        title = _title;
+    }
+    string getTitle(){
+        return title;
+    }
     
-    book1.displayBook();
+    void setPublisher (string _publisher ) {
+        publisher = _publisher;
+    }
+    string getPublisher(){
+        return publisher;
+    }
+    void setYearIssued (string _yearIssued ) {
+        yearIssued = _yearIssued;
+    }
+    string getYearIssued(){
+        return yearIssued;
+    }
+    void displayMagazine(){
+        cout << "Title: " << title << endl;
+        cout << "Publisher: " << publisher << endl;
+        cout << "Year Issued: #" << yearIssued << endl;
+        cout << "Thank you. Goodbye" << endl;
+    };
+    
+};
+
+class Library {
+private:
+    vector <VHS> allVHS;
+    vector <Book> allBooks;
+    vector <Magazine> allMagazines;
+public:
+    void addVHS(VHS _film){
+        allVHS.push_back(_film);
+    };
+    void addBook(Book _book){
+        allBooks.push_back(_book);
+    };
+    void addMagazine(Magazine _magazine){
+        allMagazines.push_back(_magazine);
+    };
+    void displayCollection(){
+        for ( auto &i : allVHS){
+            i.displayVHS();
+        }
+        for ( auto &i : allBooks){
+            i.displayBook();
+        }
+        for ( auto &i : allMagazines){
+            i.displayMagazine();
+        }
+    };
+};
+
+Library newCollection;
+
+void addToLibrary(){
+    string userInput;
+    string almostFinalInput;
+    cout << "Would you like to add a [B]ook, [V]hs, or [M]agazine?" << endl;
+    cin >> userInput;
+    if ( userInput == "B" || userInput == "b" ){
+        string newTitle;
+        string newAuthor;
+        string newDescription;
+        cin.ignore();
+        Book newBook;
+        cout << "Please enter a title: " << endl;
+        getline(cin, newTitle);
+        cout << "Please enter a author: " << endl;
+        getline(cin, newAuthor);
+        cout << "Please enter a description: " << endl;
+        getline(cin, newDescription);
+        newCollection.addBook(newBook);
+        
+        cout << "Continue adding to library [Y/N]" << endl;
+        cin >> almostFinalInput;
+        if ( almostFinalInput == "Y" || almostFinalInput == "y" ){
+            addToLibrary();
+        }
+        
+    } else if ( userInput == "V" || userInput == "v" ){
+        string newTitle;
+        string newDescription;
+        cin.ignore();
+        VHS newVHS;
+        cout << "Please enter a title: " << endl;
+        getline(cin, newTitle);
+        cout << "Please enter a description: " << endl;
+        getline(cin, newDescription);
+        newCollection.addVHS(newVHS);
+        
+        cout << "Continue adding to library [Y/N]" << endl;
+        cin >> almostFinalInput;
+        if ( almostFinalInput == "Y" || almostFinalInput == "y" ){
+            addToLibrary();
+        }
+        
+    } else if ( userInput == "M" || userInput == "m" ){
+        string newTitle;
+        string newPublisher;
+        string newYearIssued;
+        Magazine newMagazine;
+        cout << "Please enter a title: " << endl;
+        getline(cin, newTitle);
+        cout << "Please enter a publisher: " << endl;
+        getline(cin, newPublisher);
+        cout << "Please enter a issue year: " << endl;
+        getline(cin, newYearIssued);
+        newCollection.addMagazine(newMagazine);
+        
+        cout << "Continue adding to library [Y/N]" << endl;
+        cin >> almostFinalInput;
+        if ( almostFinalInput == "Y" || almostFinalInput == "y" ){
+            addToLibrary();
+        }
+        
+    } else {
+        cout << "Incorrect Input" << endl;
+        exit(0);
+    }
+};
+void start();
+
+int main(){
+    start();
     return 0;
+}
+
+void start(){
+        string userInput;
+        string finalInput;
+        
+        cout << "Would you like to add to the library? [Y/N]" << endl;
+        cin >> userInput;
+        if ( userInput == "Y" || userInput == "y" ){
+            addToLibrary();
+        } else if ( userInput == "N" || userInput == "n" ) {
+            exit(0);
+            
+        }
+    cout << "Would you like to display the library? [Y/N]" << endl;
+    if ( finalInput == "Y" || finalInput == "y" ){
+        newCollection.displayCollection();
+    } else if ( finalInput == "N" || finalInput == "n" ) {
+        cout << "Exiting Now" << endl;
+        exit(0);
+    } else { cout << "Incorrect Input"<< endl; exit(0);}
 }
